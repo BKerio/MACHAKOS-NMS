66ea764c022692107de4e45f045b3cc2dd090f5e
BKerio <briankerio48@gmail.com>
2026-08-25 22:01:26 +0300

    new update on the mobile app

--- nccg/lib/config/server.dart
+++ nccg/lib/config/server.dart
@@ -7,7 +7,7 @@
 /// # Android emulator → host machine's backend (no /api prefix)
 /// flutter run --dart-define=API_BASE_URL=http://10.0.2.2:3000
 ///
-/// # Physical device on same Wi‑Fi → your PC's LAN IPjj
+/// # Physical device on same Wi‑Fi → your PC's LAN IP
 /// flutter run --dart-define=API_BASE_URL=http://192.168.1.42:3000
 /// ```
 class Config {
