efccc078993a72adc0bf7aa87c255a9ee3c9c981
BKerio <briankerio48@gmail.com>
2026-08-25 22:27:43 +0300

    new update on the mobile app

--- nccg/lib/config/server.dart
+++ nccg/lib/config/server.dart
@@ -13,6 +13,6 @@
 class Config {
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://192.168.100.147:3000/api',
+    defaultValue: 'http://192.168.100.147:3000/api',ff
   );
 }
